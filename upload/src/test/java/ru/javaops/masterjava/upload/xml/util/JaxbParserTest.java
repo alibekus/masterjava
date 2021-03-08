e0232e3ff07f42edd7d0eb2a1043c15f50c428b4
Alibek Akbarov <alibekus@post.kz>
2021-03-08 17:34:55 +0600

    MasterJava. Lesson 3. HW. Main. Fix imports.
    Fixed wrong import packages.
    Added Google Guava dependency.

--- upload/src/test/java/ru/javaops/masterjava/upload/xml/util/JaxbParserTest.java
+++ upload/src/test/java/ru/javaops/masterjava/upload/xml/util/JaxbParserTest.java
@@ -2,9 +2,9 @@ package ru.javaops.masterjava.upload.xml.util;
 
 import com.google.common.io.Resources;
 import org.junit.Test;
-import ru.javaops.masterjava.xml.schema.CityType;
-import ru.javaops.masterjava.xml.schema.ObjectFactory;
-import ru.javaops.masterjava.xml.schema.Payload;
+import ru.javaops.masterjava.upload.xml.schema.CityType;
+import ru.javaops.masterjava.upload.xml.schema.ObjectFactory;
+import ru.javaops.masterjava.upload.xml.schema.Payload;
 
 import javax.xml.bind.JAXBElement;
 import javax.xml.namespace.QName;
